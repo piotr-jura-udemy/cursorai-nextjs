f00333e910661c4a2e8620455867c55c05b37956
Piotr Jura <jura.piotrek@gmail.com>
2025-01-14 14:09:23 +0100

    015-connecting-drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./src/lib/db/schema.ts",
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    host: process.env.POSTGRES_HOST || "localhost",
-    port: Number(process.env.POSTGRES_PORT) || 5432,
-    user: process.env.POSTGRES_USER || "postgres",
-    password: process.env.POSTGRES_PASSWORD || "postgres",
-    database: process.env.POSTGRES_DB || "kanban",
+    url: process.env.DB_URL,
   },
-} satisfies Config;
+});
